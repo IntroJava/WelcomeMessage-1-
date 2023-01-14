7e03218ed5a0bb878c3cf6772534d6c728b39f20
Emily Tumacder <emilytmdr@gmail.com>
2023-01-14 08:58:15 -0800

    Initial commit

---
+++ src/WelcomeMessage.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class WelcomeMessage {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * GOAL: Output a welcome message based on the user's selected language
+		 * EXAMPLE: 
+		 * INPUT: English
+		 * OUTPUT: Hello World
+		 * INPUT: Spanish
+		 * OUTPUT: Hola Mundo 
+		*/
+		
+		//Create an input stream
+		Scanner keyboard = new Scanner(System.in);
+		//Example: int num = keyboard.nextLine();
+		
+		//Create an input variable 
+		String language = "English";
+		
+		//Prompt the user's input
+		
+		//Set the variable equal to the user's input
+		
+		//Use if conditions to output the correct answer
+		//if(Condition){}
+		//To compare Strings: <string variable>.contentEquals("Comparison String");
+	}
+}
